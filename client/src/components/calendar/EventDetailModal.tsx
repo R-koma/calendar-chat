94db9918763d099484e9471504e5423bea321d27
R-koma <ryomakomazawa@gmail.com>
2024-09-12 23:37:01 +0900

    feature: create edit functionality and style

--- client/src/components/calendar/EventDetailModal.tsx
+++ client/src/components/calendar/EventDetailModal.tsx
@@ -10,6 +10,7 @@ import DescriptionIcon from '@mui/icons-material/Description';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
+import ChatIcon from '@mui/icons-material/Chat';
 import { EventDetail } from '@/types/Event';
 import { useEffect, useState } from 'react';
 import api from '@/utils/api';
@@ -111,6 +112,15 @@ export default function EventDetailModal({
     }
   };
 
+  useEffect(() => {
+    if (isEditing) {
+      setEventName(event.event_name);
+      setMeetingTime(event.meeting_time);
+      setMeetingPlace(event.meeting_place);
+      setDescription(event.description);
+    }
+  }, [isEditing, event]);
+
   const handleUpdateEvent = async () => {
     try {
       const csrfToken = document.cookie
@@ -128,13 +138,12 @@ export default function EventDetailModal({
         },
         { headers: { 'X-CSRF-TOKEN': csrfToken } },
       );
-      setIsEditing(false); // 編集モード終了
+      setIsEditing(false);
     } catch (err) {
       setError('イベントの更新に失敗しました');
     }
   };
 
-  // イベント削除のための関数
   const handleDeleteEvent = async () => {
     try {
       const csrfToken = document.cookie
@@ -150,7 +159,6 @@ export default function EventDetailModal({
       setError('イベントの削除に失敗しました');
     }
   };
-  console.log(user?.id, event.created_by);
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
@@ -159,43 +167,112 @@ export default function EventDetailModal({
           className="absolute top-2 right-2 cursor-pointer"
           onClick={onClose}
         />
-        <h2 className="text-md text-center mb-4 font-bold">イベント詳細</h2>
+        <div className="flex items-center justify-center mb-4">
+          <h2 className="flex justify-center text-md text-center mr-4 font-bold">
+            イベント詳細
+          </h2>
+          {user?.id === created_by && (
+            <div className="flex items-center">
+              {!isEditing && (
+                <button
+                  type="button"
+                  className="flex items-center p-1 border-none rounded bg-gray-400 text-xxs text-white h-6"
+                  onClick={() => setIsEditing(true)}
+                >
+                  <EditIcon style={{ fontSize: '16px' }} />
+                </button>
+              )}
+              <button
+                type="button"
+                className="flex items-center p-1 border-none rounded bg-red-500 text-xxs text-white h-6 ml-2"
+                onClick={handleDeleteEvent}
+              >
+                <DeleteIcon style={{ fontSize: '16px' }} />
+              </button>
+            </div>
+          )}
+        </div>
         {isEditing ? (
-          // 編集モード
           <div>
-            <input
-              type="text"
-              value={eventName}
-              onChange={(e) => setEventName(e.target.value)}
-              className="mb-2 p-1 w-full"
-            />
-            <input
-              type="text"
-              value={meetingTime}
-              onChange={(e) => setMeetingTime(e.target.value)}
-              className="mb-2 p-1 w-full"
-            />
-            <input
-              type="text"
-              value={meetingPlace}
-              onChange={(e) => setMeetingPlace(e.target.value)}
-              className="mb-2 p-1 w-full"
-            />
-            <textarea
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              className="mb-2 p-1 w-full"
-            />
-            <div className="flex justify-end space-x-2">
+            <div className="mb-2">
+              <label className="block text-xxs font-bold">
+                イベント名
+                <input
+                  type="text"
+                  className="w-full h-8 p-2 border rounded text-gray-700 text-xs outline-none"
+                  value={eventName}
+                  onChange={(e) => setEventName(e.target.value)}
+                />
+              </label>
+            </div>
+            <div className="mb-2">
+              <label className="block text-xxs font-bold">
+                時間
+                <select
+                  className="w-full h-8 p-2 border rounded text-gray-700 text-xs outline-none"
+                  value={meetingTime}
+                  onChange={(e) => setMeetingTime(e.target.value)}
+                >
+                  <option value=""></option>
+                  <option value="未定">未定</option>
+                  <option value="00:00">0:00</option>
+                  <option value="01:00">1:00</option>
+                  <option value="02:00">2:00</option>
+                  <option value="03:00">3:00</option>
+                  <option value="04:00">4:00</option>
+                  <option value="05:00">5:00</option>
+                  <option value="06:00">6:00</option>
+                  <option value="07:00">7:00</option>
+                  <option value="08:00">8:00</option>
+                  <option value="09:00">9:00</option>
+                  <option value="10:00">10:00</option>
+                  <option value="11:00">11:00</option>
+                  <option value="12:00">12:00</option>
+                  <option value="13:00">13:00</option>
+                  <option value="14:00">14:00</option>
+                  <option value="15:00">15:00</option>
+                  <option value="16:00">16:00</option>
+                  <option value="17:00">17:00</option>
+                  <option value="18:00">18:00</option>
+                  <option value="19:00">19:00</option>
+                  <option value="20:00">20:00</option>
+                  <option value="21:00">21:00</option>
+                  <option value="22:00">22:00</option>
+                  <option value="23:00">23:00</option>
+                </select>
+              </label>
+            </div>
+            <div className="mb-2">
+              <label className="block text-xxs font-bold">
+                場所
+                <input
+                  type="text"
+                  className="w-full h-8 p-2 border rounded text-gray-700 text-xs outline-none"
+                  value={meetingPlace}
+                  onChange={(e) => setMeetingPlace(e.target.value)}
+                />
+              </label>
+            </div>
+            <div className="mb-4">
+              <label className="block text-xxs font-bold">
+                説明
+                <textarea
+                  className="w-full h-12 p-2 border rounded text-gray-700 text-xxs outline-none"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+              </label>
+            </div>
+            <div className="flex justify-center space-x-2">
               <button
                 onClick={handleUpdateEvent}
-                className="bg-blue-500 text-white p-1 rounded"
+                className="flex items-center p-2 border-none rounded bg-blue-700 text-xxs text-white h-6"
               >
                 更新
               </button>
               <button
                 onClick={() => setIsEditing(false)}
-                className="bg-gray-500 text-white p-1 rounded"
+                className="flex items-center p-2 border-none rounded bg-gray-400 text-xxs text-white h-6"
               >
                 キャンセル
               </button>
@@ -302,43 +379,27 @@ export default function EventDetailModal({
               <div className="flex justify-center mt-4">
                 <button
                   type="button"
-                  className="flex items-center p-2 border-none rounded bg-blue-500 text-xxs text-white h-6"
+                  className="flex items-center p-4 border-none rounded bg-blue-500 text-xxs text-white h-6"
                   onClick={startChat}
                 >
-                  チャットを始める
+                  <ChatIcon className="mr-1" style={{ fontSize: '16px' }} />
+                  チャット
                 </button>
               </div>
             )}
           </div>
         )}
-        <div className="flex justify-center mt-4">
-          <button
-            type="button"
-            className="flex items-center p-2 border-none rounded bg-gray-400 text-xxs text-white h-6"
-            onClick={onClose}
-          >
-            閉じる
-          </button>
-          {user?.id === created_by && (
-            <div className="flex justify-center mt-4">
-              <button
-                type="button"
-                className="flex items-center p-2 border-none rounded bg-gray-400 text-xxs text-white h-6"
-                onClick={() => setIsEditing(true)} // 編集モードに切り替え
-              >
-                <EditIcon style={{ fontSize: '16px' }} /> 編集
-              </button>
-              <button
-                type="button"
-                className="flex items-center p-2 border-none rounded bg-red-500 text-xxs text-white h-6 ml-2"
-                onClick={handleDeleteEvent} // 削除関数を呼び出す
-              >
-                <DeleteIcon style={{ fontSize: '16px' }} />
-                削除
-              </button>
-            </div>
-          )}
-        </div>
+        {!isEditing && (
+          <div className="flex justify-center mt-4">
+            <button
+              type="button"
+              className="flex items-center p-2 border-none rounded bg-gray-400 text-xxs text-white h-6"
+              onClick={onClose}
+            >
+              閉じる
+            </button>
+          </div>
+        )}
       </div>
       {error && <div className="text-red-500 text-xs mt-2">{error}</div>}
     </div>
